9d9c588441370cc7eaf1d79d8796366cfdce454b
julia noda <111542735+juhnoda@users.noreply.github.com>
2024-10-08 09:36:12 -0300

    aula 04

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-    
-    const caixaPerguntas = document.querySelector(".caixa-perguntas");
-    
+
+    const caixaPerguntas = document.querySelector(".caixa-perguntas"); 
     const caixaAlternativas = document.querySelector(".caixa-alternativas");
     
     const caixaResultado = document.querySelector(".caixa-resultado");
-    
-    const textoResultado = document.querySelector(".texto-resultado");
-    
+    const textoResultado = document.querySelector(".texto-resultado"); 
     const perguntas = [
     {
     enunciado: "Pergunta 1",
@@ -17,4 +14,17 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
     enunciado: "Pergunta 2",
     alternativas: ["Alternativa 1", "Alternativa 2"],
     },
+    {
+    enunciado: "Insira o enunciado da Pergunta 3",
+    alternativas: ["Alternativa 1”, “Alternativa 2"]
+}
     ];
+    let atual = 0;
+    let perguntaAtual;
+
+    function mostraPergunta() {
+        perguntaAtual = perguntas[atual];
+        caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+mostraPergunta();
+
