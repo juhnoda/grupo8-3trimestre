87152a74d426f4b3bdad39f8c73e68b8efcaad76
julia noda <111542735+juhnoda@users.noreply.github.com>
2024-10-29 09:40:20 -0300

    a

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-
-    const caixaPerguntas = document.querySelector(".caixa-perguntas"); 
-    const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaPerguntas = document.querySelector(".caixa-perguntas"); 
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado"); 
     
-    const caixaResultado = document.querySelector(".caixa-resultado");
-    const textoResultado = document.querySelector(".texto-resultado"); 
-    const perguntas = [
+const perguntas = [
     {
     enunciado: "Pergunta 1",
     alternativas: ["Alternativa 1", "Alternativa 2"],
@@ -17,8 +16,9 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
     {
     enunciado: "Insira o enunciado da Pergunta 3",
     alternativas: ["Alternativa 1”, “Alternativa 2"]
-}
+    },
     ];
+
     let atual = 0;
     let perguntaAtual;
 
@@ -28,16 +28,10 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
         mostraAlternativas(); 
 }
 function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas)
-    const botaoAlternativas = document.createElement("button");
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
     botaoAlternativas.textContent = alternativa;
     caixaAlternativas.appendChild(botaoAlternativas);
-    perguntaAtual = perguntas[atual];
-caixaPerguntas.textContent = perguntaAtual.enunciado;
-mostraPergunta();
+   
+    }
 }
-const perguntas = [
-    {
-    enunciado: "Insira o enunciado da Pergunta 1",
-    alternativas: ["Alternativa 1”, “Alternativa 2"],
-    },
